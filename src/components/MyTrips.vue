96376117079ce6f39731df7bc4754a5dc46fbe32
Sylwia Laskowska <sylwia_lask@wp.pl>
2020-09-07 23:52:30 +0200

    trips display

--- src/components/MyTrips.vue
+++ src/components/MyTrips.vue
@@ -1,9 +1,24 @@
 <template>
+  <div>
     <h2>My trips</h2>
+    <div class="row">
+      <single-trip v-for="trip in trips" :key="trip.id" class="col-sm-4" :trip="trip">{{trip.country}}</single-trip>
+    </div>
+  </div>
 </template>
 
 <script>
+import { myTrips } from "../data";
+import SingleTrip from "./SingleTrip";
+
 export default {
-    name: "MyTrips"
-}
+  name: "MyTrips",
+  components: { SingleTrip },
+  data: () => ({
+    trips: [],
+  }),
+  mounted() {
+    this.trips = myTrips;
+  },
+};
 </script>
---
+++ src/components/SingleTrip.vue
@@ -0,0 +1,33 @@
+<template>
+  <b-card
+    :title="trip.country"
+    :img-src="trip.photoLink"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 20rem; "
+    class="mb-5 centered"
+  >
+    <b-card-text>
+      <ul>
+        <li v-for="place in trip.seen" :key="place">{{place}}</li>
+      </ul>
+    </b-card-text>
+  </b-card>
+</template>
+
+<script>
+export default {
+  name: "SingleTrip",
+  props: ["trip"],
+};
+</script>
+
+<style scoped>
+.centered {
+  margin: 0 auto;
+}
+.card-img-top {
+    max-height: 180px;
+}
+</style>
